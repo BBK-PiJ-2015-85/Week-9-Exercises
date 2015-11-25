c8afb4cc26aece403f692f15c929236067c83204
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2015-11-25 11:16:28 +0000

    interface for question 2

---
+++ IntSet.java
@@ -0,0 +1,22 @@
+public interface IntSet {
+	/**
+	*adds a new int to the set; if it is there already, nothing happens.
+	*/	
+	void add(int newInt);
+		
+	/**
+	* returns true if the number is in the set, false otherwise.
+	*/
+	boolean contains(int num);
+	
+	/**
+	*returns the same values as the former method, 
+	*but for every element that is checked prints its value on screen.
+	*/
+	boolean containsVerbose(int num);
+	
+	/**
+	*returns a string with the values of the elements in the set separated by commas.
+	*/
+	String toString(); 
+}
